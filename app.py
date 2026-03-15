34b768e775249e8ebbf010eb47d69edb4e7facaf
kiranreddy0101 <148518139+kiranreddy0101@users.noreply.github.com>
2026-03-15 16:04:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2310,7 +2310,7 @@ def get_weather(city_name, lang):
 @st.cache_resource
 def load_resources():
     try:
-        disease_model = load_model("plant_disease_model_final4.h5", compile=False)
+        disease_model = load_model("training_outputs/plant_disease_mobilenetv2.h5", compile=False)
     except Exception:
         disease_model = None
 
